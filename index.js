8ff9c108aeb0b7e696b71f9d177d1ef587a7b243
Kirill Avdonin <96391186+Kiryann2114@users.noreply.github.com>
2024-04-16 00:16:46 +0300

    update

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ const mysql = require('mysql2');
 async function sendMail(email, theme, text, textHtml) {
 
     let transporter = nodemailer.createTransport({
-        host: "mail.godinecoffee.ru",
+        host: "smtp-mail.godinecoffee.ru",
         port: 587,
         secure: false,
-        ignoreTLS: true,
+        requireTLS: true,
         logger: true,
         debug: true,
         auth: {
             user: 'info@godinecoffee.ru',
             pass: 'KKiriLL2114'
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
