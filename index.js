fe59f59700e01e5af90209593d55c234a8935557
Kirill Avdonin <96391186+Kiryann2114@users.noreply.github.com>
2024-04-16 22:39:13 +0300

    update

--- index.js
+++ index.js
@@ -198,4 +198,5 @@ app.post('/api/SendMailReset', async (req, res) => {
     let textHtml = "<b>Привет мир!</b>"
     console.log(req.body.mail)
     await sendMail(req.body.mail, 'Message from Node js', 'This message was sent from Node js server.', textHtml)
+    res.json({status:"ok"});
 });
