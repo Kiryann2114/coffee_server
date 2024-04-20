24ccded19eaa0b3698295b59a931edf9c1c971cc
Kirill Avdonin <96391186+Kiryann2114@users.noreply.github.com>
2024-04-20 13:31:57 +0300

    update

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/api/tovar',(req,res) => {
 app.use(express.json());
 
 app.post('/api/loginAdmin', (req, res) => {
-    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
 
     connsql.query(query,(err,result,field) => {
         res.json(result[0]);
@@ -88,7 +88,7 @@ app.post('/api/loginAdmin', (req, res) => {
 });
 
 app.post('/api/deleteItem', (req, res) => {
-    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     connsql.query(query,(err,result,field) => {
         if(result[0]){
             let query1 = "DELETE FROM Tovar WHERE Tovar.id = " + req.body.id;
@@ -98,7 +98,7 @@ app.post('/api/deleteItem', (req, res) => {
 });
 
 app.post('/api/addItem', (req, res) => {
-    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     connsql.query(query,(err,result,field) => {
         if(result[0]){
             let query1 = 'SELECT MAX(id)+1 as ID FROM Tovar';
@@ -111,7 +111,7 @@ app.post('/api/addItem', (req, res) => {
 });
 
 app.post('/api/UpdateItem', (req, res) => {
-    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'select count(*) <> 0 as res from users where id = 0 and mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     connsql.query(query,(err,result,field) => {
         if(result[0]){
             let query1 = "UPDATE Tovar SET name = '" + req.body.name + "', opisanie = '" + req.body.opisanie + "' ,price = '" + req.body.price + "', optprice = '" + req.body.optprice + "' WHERE Tovar.id = " + req.body.id;
@@ -135,7 +135,7 @@ app.post('/api/CountBasket', (req, res) => {
 });
 
 app.post('/api/checkUser', (req, res) => {
-    let query = 'select count(*) <> 0 as res from users where mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'select count(*) <> 0 as res from users where mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
 
     connsql.query(query,(err,result,field) => {
         res.json(result[0]);
@@ -143,12 +143,12 @@ app.post('/api/checkUser', (req, res) => {
 });
 
 app.post('/api/RegUser', (req, res) => {
-    let query = 'select count(*) = 0 as res from users where mail = "' + req.body.mail + '"';
+    let query = 'select count(*) = 0 as res from users where mail = "' + req.body.mail.toLowerCase() + '"';
     connsql.query(query,(err,result,field) => {
         if(result[0]){
             let query1 = 'select max(id)+1 as res from users';
             connsql.query(query1,(err,resu,field) => {
-                let query2 = "INSERT INTO users (password, name, mail, tel, addres, korzina, liked, id) VALUES ('"+ md5(req.body.pass) +"', '"+ req.body.name +"', '"+ req.body.mail +"', '"+ req.body.tel +"', '', '', '', '"+ resu[0].res +"')"
+                let query2 = "INSERT INTO users (password, name, mail, tel, addres, korzina, liked, id) VALUES ('"+ md5(req.body.pass) +"', '"+ req.body.name +"', '"+ req.body.mail.toLowerCase() +"', '"+ req.body.tel +"', '', '', '', '"+ resu[0].res +"')"
                 connsql.query(query2,(err,resu,field) => {
                     res.json(result[0]);
                 })
@@ -158,24 +158,24 @@ app.post('/api/RegUser', (req, res) => {
 });
 
 app.post('/api/UpdateBasket', (req, res) => {
-    let query = 'select count(*) <> 0 as res from users where mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'select count(*) <> 0 as res from users where mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     connsql.query(query,(err,result,field) => {
         if(result[0]){
-            let query1 = 'UPDATE users SET korzina = "' + req.body.value + '" WHERE mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+            let query1 = 'UPDATE users SET korzina = "' + req.body.value + '" WHERE mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
             connsql.query(query1)
         }
     })
 });
 
 app.post('/api/GetBasket', (req, res) => {
-    let query = 'Select korzina as res from users WHERE mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'Select korzina as res from users WHERE mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     connsql.query(query,(err,result,field) => {
         res.json(result[0]);
     })
 });
 
 app.post('/api/GetInfoUser', (req, res) => {
-    let query = 'Select name, mail, tel from users WHERE mail = "' + req.body.mail + '" and password = "' + md5(req.body.pass) + '"';
+    let query = 'Select name, mail, tel from users WHERE mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     connsql.query(query,(err,result,field) => {
         res.json(result[0]);
     })
@@ -184,17 +184,41 @@ app.post('/api/GetInfoUser', (req, res) => {
 app.post('/api/UpdateInfoUser', (req, res) => {
     let query = ""
     if(req.body.pass !== "" && req.body.passp !== ""){
-        query = 'UPDATE users SET password = "' + md5(req.body.passp) + '",name = "' + req.body.name + '",mail = "' + req.body.mail + '",tel = "' + req.body.tel + '" WHERE mail = "' + req.body.maillog + '" and password = "' + md5(req.body.pass) + '"';
+        query = 'UPDATE users SET password = "' + md5(req.body.passp) + '",name = "' + req.body.name + '",mail = "' + req.body.mail.toLowerCase() + '",tel = "' + req.body.tel + '" WHERE mail = "' + req.body.maillog.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
     }
     else {
-        query = 'UPDATE users SET name = "' + req.body.name + '",mail = "' + req.body.mail + '",tel = "' + req.body.tel + '" WHERE mail = "' + req.body.maillog + '" and password = "' + md5(req.body.passlog) + '"';
+        query = 'UPDATE users SET name = "' + req.body.name + '",mail = "' + req.body.mail.toLowerCase() + '",tel = "' + req.body.tel + '" WHERE mail = "' + req.body.maillog.toLowerCase() + '" and password = "' + md5(req.body.passlog) + '"';
     }
     connsql.query(query)
 });
 
 app.post('/api/SendMailReset', async (req, res) => {
-    let textHtml = "<b>Привет мир!</b>"
-    console.log("send email " + req.body.mail)
-    await sendMail(req.body.mail, 'Message from Node js', 'This message was sent from Node js server.', textHtml)
+    console.log("send email " + req.body.mail.toLowerCase())
+    let query1 = 'UPDATE users SET korzina = "' + req.body.value + '" WHERE mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
+    connsql.query(query1)
+
+    let textHtml = "<link>{}</link>"
+
+
+    await sendMail(req.body.mail.toLowerCase(), 'Восстановление пароля', 'Это сообщение отправлено для восстановления пароля.', textHtml)
     res.json({status:"ok"});
+});
+
+app.post('/api/ResetPass', async (req, res) => {
+    if(req.body.url !== "0" && req.body.url !== ""){
+
+        let query = 'select count(*) <> 0 as res from users where mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"'
+
+        connsql.query(query,(err,result,field) => {
+            if(result[0]){
+
+                let query1 = 'UPDATE users SET korzina = "' + req.body.value + '" WHERE mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
+                connsql.query(query1)
+
+                let query2 = 'UPDATE users SET korzina = "' + req.body.value + '" WHERE mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
+                connsql.query(query2)
+
+            }
+        })
+    }
 });
