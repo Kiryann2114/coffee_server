bdb3ad12a25ed56947b7f2e0c49790b36e674da1
Kirill Avdonin <96391186+Kiryann2114@users.noreply.github.com>
2024-04-22 17:09:14 +0300

    update

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ app.post('/api/GetPaymentURL', (req, res) => {
 
     const url = 'https://api.yookassa.ru/v3/payments';
     const base64Credentials = Buffer.from('369984:test_3l-27_egpYA4GB8lsVLx1W5QxR0CGDxRQLG6X_VMHvk').toString('base64');
-    const idempotenceKey = '<0000001assadada>';
+    const idempotenceKey = '0000001assadada';
     const requestData = {
         amount: { value: '100.00', currency: 'RUB' },
         capture: true,
