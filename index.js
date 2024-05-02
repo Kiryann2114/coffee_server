eb960110d37e4db5dc2a9b9e09063bef564e75ad
Kirill Avdonin <96391186+Kiryann2114@users.noreply.github.com>
2024-05-02 16:37:08 +0300

    update

--- index.js
+++ index.js
@@ -283,7 +283,6 @@ app.post('/api/GetPaymentURL', (req, res) => {
 
             let Item = arrBasket[i].split(":")
 
-            console.log(result[i].price + '.00')
             arrItems.push({
                 description:result[i].name,
                 amount: { value: result[i].price + '.00', currency: 'RUB' },
@@ -297,7 +296,6 @@ app.post('/api/GetPaymentURL', (req, res) => {
         const url = 'https://api.yookassa.ru/v3/payments';
         const base64Credentials = Buffer.from('369984:test_3l-27_egpYA4GB8lsVLx1W5QxR0CGDxRQLG6X_VMHvk').toString('base64');
         const idempotenceKey = fastRandString();
-        console.log(arrItems);
         const requestData = {
             amount: { value: String( Number(req.body.baskCount) + Number(req.body.delprice)) + '.00', currency: 'RUB' },
             capture: true,
