c8d545135537eff2f205cc37c4e203f33ae4c2a9
Kirill Avdonin <96391186+Kiryann2114@users.noreply.github.com>
2024-10-21 17:43:53 +0300

    update

--- index.js
+++ index.js
@@ -278,15 +278,44 @@ app.post('/api/GetPaymentURL', (req, res) => {
 
     let query = 'SELECT price,price250,name FROM Tovar WHERE id in (' + arrID + ')'
 
+    let query1 = 'select sale as res from PromoCods where code = "' + req.body.Promo.toUpperCase() + '"';
+
+    let query2 = 'select count(*) <> 0 as res from PromoCods where code = "' + req.body.Promo.toUpperCase() + '"';
+
+    let query3 = 'select HistoryPromo as res from users where mail = "' + req.body.mail.toLowerCase() + '" and password = "' + md5(req.body.pass) + '"';
+
+
     connsql.query(query,(err,result,field) => {
+        let PromoSale = 1;
+
+        if(req.body.Promo!==""){
+            connsql.query(query2,(err,result2,field) => {
+                if(result2[0].res!==0){
+                    connsql.query(query3,(err,result3,field) => {
+                        let arr = result3[0].res.split(' ')
+                        let r = false;
+                        for (let i = 0; i < arr.length; i++) {
+                            if (arr[i] === req.body.Promo) {
+                                r = true;
+                            }
+                        }
+                        if(!r){
+                            connsql.query(query1,(err,result1,field) => {
+                                PromoSale = 1-(Number(result1[0].res)*0.01);
+                            })
+                        }
+                    })
+                }
+            })
+        }
+
         for(let i = 0; i < arrBasket.length; i++) {
 
-            /////////////////////////////////////////////////////////Тут надо добаввить процент скидки
             let Item = arrBasket[i].split(":")
             if(arrBasket[i].split(":")[0].split("_")[1] === "1"){
                 arrItems.push({
                     description:result[i].name,
-                    amount: { value: result[i].price + '.00', currency: 'RUB' },
+                    amount: { value: String(Number(result[i].price) * PromoSale) + '.00', currency: 'RUB' },
                     vat_code:1,
                     quantity:Item[1],
                     measure:'piece',
@@ -297,7 +326,7 @@ app.post('/api/GetPaymentURL', (req, res) => {
             else {
                 arrItems.push({
                     description:result[i].name,
-                    amount: { value: result[i].price250 + '.00', currency: 'RUB' },
+                    amount: { value: String(Number(result[i].price250) * PromoSale) + '.00', currency: 'RUB' },
                     vat_code:1,
                     quantity:Item[1],
                     measure:'piece',
@@ -311,7 +340,7 @@ app.post('/api/GetPaymentURL', (req, res) => {
         const idempotenceKey = fastRandString();
         console.log(arrItems);
         const requestData = {
-            amount: { value: String( Number(req.body.baskCount) + Number(req.body.delprice)) + '.00', currency: 'RUB' },
+            amount: { value: String( (Number(req.body.baskCount) * PromoSale) + Number(req.body.delprice)) + '.00', currency: 'RUB' },
             capture: true,
             confirmation: {
                 type: 'redirect',
